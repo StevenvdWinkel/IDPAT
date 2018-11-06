600e52be466d2bdf9738cd8abdf87cf6c9b3a8ec
Steven van de Winkel <s.vandewinkel1@student.avans.nl>
2018-11-06 12:30:34 +0100

    DPINT complete with new files added

---
+++ 2018/DPINT_Wk1_Strategies-master/HexadecimelNumberConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPINT_Wk1_Strategies
+{
+    class HexadecimelNumberConverter : INumberConverter
+    {
+        public string ToLocalString(int fromNumber)
+        {
+            return Convert.ToString(fromNumber, 16);
+        }
+
+        public int ToNumerical(string FromText)
+        {
+            return Convert.ToInt32(FromText, 16);
+        }
+    }
+}
---
+++ 2018/DPINT_Wk1_Strategies-master/NumberConverterFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPINT_Wk1_Strategies
+{
+    class NumberConverterFactory
+    {
+        public IEnumerable<string> ConverterNames
+        {
+            get { return _converters.Keys; }
+        }
+
+        private Dictionary<string, INumberConverter> _converters;
+
+        public NumberConverterFactory()
+        {
+            _converters = new Dictionary<string, INumberConverter>();
+
+            _converters["Numerical"] = new NumericalNumberConverter();
+            _converters["Binary"] = new BinaryNumberConverter();
+            _converters["Roman"] = new RomanNumberConverter();
+            _converters["Hexadecimal"] = new HexadecimelNumberConverter();
+            _converters["Octal"] = new OctalNumberConverter();
+        }
+
+        public INumberConverter GetNumberConverter(string name)
+        {
+            return _converters[name];
+        }
+    }
+}
